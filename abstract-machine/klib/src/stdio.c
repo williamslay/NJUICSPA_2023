99e5719aa636caa4708b1e56e158a5c31ba0d5e5
williamslay_linux <williamslay.lsy@gmail.com>
2024-04-29 22:36:44 +0800

    pa2.2.1 some unreliable implementation

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,68 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+char * intToCharArray(int num, char *result) {
+  int i = 0;
+  int isNegative = 0;
+  // Handle negative numbers
+  if (num < 0) {
+    isNegative = 1;
+    num = -num;
+  }
+  // Convert digits to characters in reverse order
+  do {
+    result[i++] = num % 10 + '0';
+    num /= 10;
+  } while (num > 0);
+  // Add negative sign if necessary
+  if (isNegative) {
+    result[i++] = '-';
+  }
+  // Reverse the character array
+  int start = 0;
+  int end = i - 1;
+  while (start < end) {
+    char temp = result[start];
+    result[start] = result[end];
+    result[end] = temp;
+    start++;
+    end--;
+  }
+  return result + i ;
+}
+/*a function dosen't deal with error , just pass the hello-str.c test*/ 
+int sprintf(char *out, const char *fmt, ...) { 
+  char * temp = out;
+  va_list ap;
+  va_start(ap, fmt);
+  while (*fmt != '\0') {
+    switch (*fmt) {
+      case '%' : 
+        if(*(fmt-1) == '%') *temp++ = *fmt++;
+        else fmt++;
+        break;
+      case 's':              /* string  or 's'*/
+        if(*(fmt-1) == '%') {
+          char *s = va_arg(ap, char *);
+          int i = 0,len = strlen(s);
+          while(i<len) 
+            *temp++ = s[i++];
+          fmt++;
+        } else *temp++ = *fmt++; 
+        break;
+      case 'd':              /* int or 'd' */
+        if(*(fmt-1) == '%') {
+          int num = va_arg(ap, int);
+          temp = intToCharArray(num , temp) ;
+          fmt++;
+        } else *temp++ = *fmt++;  
+        break;
+      default :  *temp++ = *fmt++; break;
+    }
+  }
+  *temp = '\0';
+  va_end(ap);
+  return strlen(out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,91 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i = 0;
+  while(s[i] != '\0') i++;
+  return i;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t i = 0;
+  while(src[i] != '\0') {
+    dst[i] = src[i];
+    i++;
+  }
+  return dst; 
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dst[i] = src[i];
+  for ( ; i < n; i++)
+    dst[i] = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dest_len = strlen(dst);
+  size_t i;
+  for (i = 0 ; src[i] != '\0' ; i++)
+    dst[dest_len + i] = src[i];
+  dst[dest_len + i] = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  size_t s1_val = 0,s2_val = 0;
+  size_t i;
+  for (i = 0 ; s1[i] != '\0' ; i++) 
+    s1_val += s1[i];
+  for (i = 0 ; s2[i] != '\0' ; i++) 
+    s2_val += s2[i]; 
+  return s1_val - s2_val;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t s1_val = 0,s2_val = 0;
+  size_t i;
+  for (i = 0 ; i < n && s1[i] != '\0' ; i++) 
+    s1_val += s1[i];
+  for (i = 0 ; i < n && s2[i] != '\0' ; i++) 
+    s2_val += s2[i]; 
+  return (int) (s1_val - s2_val);
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  char * t = s;
+  for(i = 0 ; i < n ; i++) 
+    t[i] = c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  unsigned char tmp[n];
+  memcpy(tmp,src,n);
+  memcpy(dst,tmp,n);
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char *dp = out;
+  const char *sp = in;
+  while (n--)
+    *dp++ = *sp++;
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  const char *t1 =  s1 , *t2 =  s2;
+  int val = 0;
+  for(i = 0 ; i < n ; i++) 
+  {
+    val = (int) (t1[i] - t2[i]);
+    if(val) return val;
+  } 
+  return 0;
 }
 
 #endif
