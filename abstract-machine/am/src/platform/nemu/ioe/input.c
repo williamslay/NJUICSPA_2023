5b5b71cbdf6d6cfa2866f5ad29edf043b09d16b9
williamslay <williamslay.lsy@gmail.com>
2025-07-05 22:23:11 +0800

    feat: keyboard finish

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t scan_code = inl(KBD_ADDR);
+  kbd->keycode = scan_code & ~KEYDOWN_MASK;
+  kbd->keydown = (scan_code & KEYDOWN_MASK) != 0;
 }
