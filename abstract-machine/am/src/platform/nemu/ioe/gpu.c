d9f290133a51ca9839e6b08428157a43323bc1ec
williamslay <williamslay.lsy@gmail.com>
2025-07-08 00:14:57 +0800

    feat: :pa2.3.4 finish vga

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,22 +2,35 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define VGA_CTL_MASK 0x0000ffff
+
+static uint16_t screen_width = 0;
+static uint16_t screen_height = 0;
 
 void __am_gpu_init() {
+  screen_width = inl(VGACTL_ADDR) >> 16;
+  screen_height = inl(VGACTL_ADDR) & VGA_CTL_MASK;
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = screen_width,
+    .height = screen_height,
+    .vmemsz = screen_width * screen_height * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  if (ctl->pixels) {
+    for (int j = 0; j < ctl->h; j++) {
+      for (int i = 0; i < ctl->w; i++) {
+        fb[(ctl->y + j) * screen_width + (ctl->x + i)] = ((uint32_t *)ctl->pixels)[j * ctl->w + i];
+      }
+    }
   }
+  if (ctl->sync) outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -33,6 +33,7 @@ static uint32_t screen_size() {
 
 static void *vmem = NULL;
 static uint32_t *vgactl_port_base = NULL;
+static uint32_t *vgasync_port_base = NULL;
 
 #ifdef CONFIG_VGA_SHOW_SCREEN
 #ifndef CONFIG_TARGET_AM
@@ -71,18 +72,28 @@ static inline void update_screen() {
 #endif
 #endif
 
-void vga_update_screen() {
-  // TODO: call `update_screen()` when the sync register is non-zero,
-  // then zero out the sync register
+void vga_update_screen(uint32_t offset, int len, bool is_write) {
+  if (is_write) {
+    // If sync register is zero, do nothing
+    if (vgasync_port_base[0] == 0) {
+      return;
+    }
+    IFDEF(CONFIG_VGA_SHOW_SCREEN, update_screen());
+    vgasync_port_base[0] = 0; // Reset sync register
+  }
 }
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
+  vgasync_port_base = vgactl_port_base + 1;
+  vgasync_port_base[0] = 0; // Initialize sync register to zero
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
+  add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 4, NULL);
+  add_pio_map ("vgasync", CONFIG_VGA_SYNC_PORT, vgasync_port_base, 4, vga_update_screen);
 #else
-  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
+  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 4, NULL);
+  add_mmio_map("vgasync", CONFIG_VGA_SYNC_MMIO, vgasync_port_base, 4, vga_update_screen);
 #endif
 
   vmem = new_space(screen_size());
